b88595275ed868040c534038236ae8814cae73a2
Carlos Henrique Brito Malta Leão <carlosbmaltaleao@gmail.com>
2025-11-24 23:09:35 -0300

    feat: remove unused files

--- classification_model/__init__.py
+++
@@ -1,9 +0,0 @@
-from data_loader import process_daic_data
-from model_trainer import train_model 
-
-if __name__ == "__main__":
-  data_dir = "./daic_data/"
-  df = process_daic_data(data_dir)
-  print(df.head())
- 
-  train_model(df)
